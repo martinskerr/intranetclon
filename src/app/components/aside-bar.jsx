01dbd7cde395bbbe7b8cd424aa9c174a10045a80
martin lopez <martinxx442@gmail.com>
2023-08-25 19:20:42 -0400

    aside bar add

---
+++ src/app/components/aside-bar.jsx
@@ -0,0 +1,63 @@
+'use client'
+
+
+
+import { useState } from "react";
+
+export function AsideBar() {
+    const [open, setOpen] = useState(true);
+    const Menus = [
+        { title: "Portal" },
+        { title: "Mi ficha" },
+        { title: "Mis encuestas" },
+        { title: "Directorio " },
+        { title: "Configuracion" }
+    ];
+
+    return (
+        <div className="flex">
+            <div
+                className={`${
+                    open ? "w-72" : "w-20 "
+                } bg-green-800 h-screen p-5 pt-8 relative duration-300`}
+            >
+                <img
+                    
+                    
+                    
+                    className={`absolute cursor-pointer -right-3 top-9 w-5 h-4 
+                    border-2  ${!open}`}
+                    onClick={() => setOpen(!open)}
+                />
+
+                <div className="flex gap-x-4 items-center">
+                    <h1
+                        className={`text-white origin-left font-medium text-xl duration-200 ${
+                            !open && "scale-0"
+                        }`}
+                    >
+                        Mi empresa
+                    </h1>
+                </div>
+                <ul className="pt-6">
+                    {Menus.map((Menu, index) => (
+                        <li
+                            key={index}
+                            className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+                            ${Menu.gap ? "mt-9" : "mt-2"} ${
+                                index === 0 && "bg-light-white"
+                            } `}
+                        >
+                            <span className={`${!open && "hidden"} origin-left duration-200`}>
+                                {Menu.title}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="h-screen flex-1 p-7">
+                <h1 className="text-2xl font-semibold">Intranet Clon</h1>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import { AsideBar } from "./components/aside-bar"
+
+export default function Home() {
+  return (
+
+    <body className="bg-slate-100">
+      <AsideBar/>
+
+      
+    </body>
+
+
+  )
+}
